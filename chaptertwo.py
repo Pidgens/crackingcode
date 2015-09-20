463a841560218eec7fbef1340271cc65f7cb044a
U-DEREKCHIU\Derek Chiu <dederekchiu@berkeley.edu>
2015-09-19 17:58:01 -0700

    stacks and queues

--- chaptertwo.py
+++ chaptertwo.py
@@ -53,17 +53,56 @@ class LinkedList(object):
         else:
             previous.set_next(current.get_next)
 
-    def deleteDuplicates(self):
-        # Array to keep track of all entries.
-        previousTable = []
-        previous = None
-        current = self.head
-        while (current):
-            previousTable.append(current)
-            if (current.get_next() in previousTable):
-                previous = current
-                current = current.get_next().get_next()
-                previous.set_next(current)
-            else:
-                previous = current
-                current = current.get_next()
+# Using a buffer.
+def deleteDuplicatesBuffer(linkedlist):
+    # Array to keep track of all entries.
+    previousTable = []
+    previous = None
+    current = linkedlist.head
+    while (current):
+        previousTable.append(current)
+        if (current.get_next() in previousTable):
+            previous = current
+            current = current.get_next().get_next()
+            previous.set_next(current)
+        else:
+            previous = current
+            current = current.get_next()
+
+# Without a buffer - Use a runner.
+def deleteDuplicates(linkedlist):
+    previous = None
+    current = linkedlist.head
+    while (current):
+        runner = linkedlist.head
+        while (runner != current):
+            if (runner.get_data() == current.get_data()):
+                tmp = current.get_next
+                previous.set_next(tmp)
+                break
+            runner = runner.get_next()
+        if (runner == current):
+            previous = current
+            current = current.get_next()
+
+# Find the n'th last element of a singly linked list.
+def findnthElement(linkedlist, n):
+    p1 = linkedlist.head
+    p2 = linkedlist.head
+    for i in range(0,n):
+        if (p2 == None):
+            # Because size < n
+            return None
+        p2 = p2.get_next()
+    # Do this by - set p2 to n out of the size of linked list
+    # Then is essentially size - n and p1 moves with it
+    while (p2.get_next is not None):
+        p2 = p2.get_next()
+        p1 = p1.get_next()
+    return p1
+
+# Delete node given only that node.
+def deleteMiddle(n):
+    next = n.get_next()
+    n.data = next.get_data()
+    n.get_next = next.get_next()
